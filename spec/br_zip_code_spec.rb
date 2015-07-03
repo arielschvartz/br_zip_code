cad6285698c182bc554ce85b5cc07c4d82ecf963
Ariel Schvartz <ari.shh@gmail.com>
2015-07-03 02:11:19 -0300

    Mais testes

--- spec/br_zip_code_spec.rb
+++ spec/br_zip_code_spec.rb
@@ -6,7 +6,7 @@ describe BrZipCode do
   end
 
   # RESPOND TO
-  [:timeout, :attempts, :timeout=, :attempts=, :configuration, :services].each do |attr|
+  [:timeout, :attempts, :timeout=, :attempts=, :configuration, :services, :strip, :formatted].each do |attr|
     it "should respond to #{attr}" do
       respond_to attr
     end
@@ -29,6 +29,15 @@ describe BrZipCode do
       BrZipCode::Service::RepublicaVirtual,
       BrZipCode::Service::CepLivre
     ])
+  end
+
+  # FORMATTED METHOD
+  describe 'the formatted method' do
+    ['22441090', '22.441-090', '22.441090', '22441-090'].each do |zip_code|
+      it "with the zip code #{zip_code} should be formatted" do
+        expect(BrZipCode.formatted(zip_code)).to eq('22.441-090')
+      end
+    end
   end
 
   # CONFIGURE NEW VALUES FOR CONFIG
